abd1700805a3973b7468fa8201fcf04fc930ce65
NTL0210 <02102004ntl@gmail.com>
2026-03-09 09:49:10 +0700

    Task 1 completed add tinhtoantru

--- Test1.js
+++ Test1.js
@@ -1,9 +1,15 @@
-async function tinhtoan(a,b) {
+async function tinhtoancong(a,b) {
     return a+b;
+}
+async function tinhtoantru(a,b) {
+    return a-b;
 }
 
 async function main() {
-    let kq = await tinhtoan(5,6);
+    let kq = await tinhtoancong(5,6);
     console.log(kq);
+    let kq2 = await tinhtoantru(40,4);
+    console.log(kq2);
 }
+
 main();
